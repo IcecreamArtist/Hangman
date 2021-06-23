e46cc61e3911fff494dcc9afe91c088eebf09df4
Artist <1443885711@qq.com>
2021-06-23 20:11:41 +0800

    first commit

--- src/com/example/Hangman/GUI_Hangman.java
+++ src/com/example/Hangman/GUI_Hangman.java
@@ -1,4 +1,11 @@
 package com.example.Hangman;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.event.*;
+import javax.swing.*;
+
+
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -10,56 +17,72 @@ import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.Scanner;
 
-public class GUI_Hangman extends JApplet implements KeyListener {
+public class GUI_Hangman extends JApplet{
+
+    String s;
+
     public void init() {
-    //Execute a job on the event-dispatching thread:
-    //creating this applet's GUI.
-    try {
-        javax.swing.SwingUtilities.invokeAndWait(new Runnable() {
-            public void run() {
-                createGUI();
-            }
-        });
-    } catch (Exception e) {
-        System.err.println("createGUI didn't successfully complete");
+        //Execute a job on the event-dispatching thread:
+        //creating this applet's GUI.
+        try {
+            javax.swing.SwingUtilities.invokeAndWait(new Runnable() {
+                public void run() {
+                    createGUI();
+                }
+            });
+        } catch (Exception e) {
+            System.err.println("createGUI didn't successfully complete");
+        }
     }
-}
 
     private void createGUI() {
-        JLabel label = new JLabel(
-                "You are successfully running a Swing applet!");
-        label.setHorizontalAlignment(JLabel.CENTER);
-        label.setBorder(BorderFactory.createMatteBorder(200,200,1,1,Color.black));
+        getContentPane().addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                if ( c != KeyEvent.CHAR_UNDEFINED ) {
+                    s = s + c;
+                    repaint();
+                    e.consume();
+                    System.out.println("test");
+                }
+            }
 
-        addKeyListener(this);
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+
+
         DisplayGraphics m = new DisplayGraphics();
-        getContentPane().add(m);
+      //  getContentPane().add(m);
 
+        JButton button = new JButton("test");
+        button.setSize(50,50);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // testNewWindow();
+            }
+        });
 
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
+        JPanel panel = new JPanel();
+        panel.add(m);
+        setContentPane(panel);
 
     }
+
 }
 
 class DisplayGraphics extends Canvas{
     public void paint(Graphics g){
-       // getGraphics().drawImage(image,0,0,getWidth(),getHeight(),null);
-        // 下面的没用
         setBackground(Color.black);
-
         // basic part
         g.setColor(Color.RED);
         g.drawLine(100, 30, 100, 270);
---
+++ src/com/example/Hangman/MyFrame.java
@@ -0,0 +1,7 @@
+package com.example.Hangman;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.net.URL;
+
